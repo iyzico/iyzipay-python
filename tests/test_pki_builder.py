dfd77485845211476511d053ecdbb33ea75da709
Abdullah Selek <abdullahselek@gmail.com>
2018-01-04 18:42:58 +0100

    Add tests for append_array

--- tests/test_pki_builder.py
+++ tests/test_pki_builder.py
@@ -32,3 +32,19 @@ class PKIBuilderTest(unittest.TestCase):
         pki_builder = iyzipay.PKIBuilder('')
         pki_builder.append_price('price', '')
         self.assertEqual(pki_builder.get_request_string(), '[]')
+
+    def test_append_array_when_value_available(self):
+        enabledInstallments = ['2', '3', '6', '9']
+        pki_builder = iyzipay.PKIBuilder('')
+        pki_builder.append_array('data', enabledInstallments)
+        self.assertEqual(pki_builder.get_request_string(), '[data=[2, 3, 6, 9]]')
+
+    def test_append_array_when_value_not_available(self):
+        pki_builder = iyzipay.PKIBuilder('')
+        pki_builder.append_array('data')
+        self.assertEqual(pki_builder.get_request_string(), '[]')
+
+    def test_append_array_when_value_empty(self):
+        pki_builder = iyzipay.PKIBuilder('')
+        pki_builder.append_array('data', [])
+        self.assertEqual(pki_builder.get_request_string(), '[]')
