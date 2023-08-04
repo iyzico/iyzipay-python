f3448aad3f848d13d1d079d3a9b8f2574359e2f9
Bünyamin Yaşar <33955141+byasarcse@users.noreply.github.com>
2023-08-04 14:07:35 +0300

    removes unneccessary callbackURL parameter from payment request (#87)
    * removes unneccessary callbackURL parameter from payment request
    
    removes unneccessary callbackURL parameter from payment request
    
    Signed-off-by: Bünyamin Yaşar <33955141+byasarcse@users.noreply.github.com>
    
    * removes callback url
    
    ---------
    
    Signed-off-by: Bünyamin Yaşar <33955141+byasarcse@users.noreply.github.com>
    Co-authored-by: recai.bostanci <recai.bostanci@iyzico.com>

--- iyzipay/iyzipay_resource.py
+++ iyzipay/iyzipay_resource.py
@@ -301,7 +301,6 @@ class Payment(IyzipayResource):
         pki_builder.append('posOrderId', request.get('posOrderId'))
         pki_builder.append('connectorName', request.get('connectorName'))
         pki_builder.append('plusInstallmentUsage', request.get('plusInstallmentUsage'))
-        pki_builder.append('callbackUrl', request.get('callbackUrl'))
         return pki_builder.get_request_string()
 
     def to_pki_string_retrieve(self, request):
@@ -776,7 +775,6 @@ class BasicThreedsInitialize(IyzipayResource):
         pki_builder.append('paymentCard', self.payment_card_pki(request.get('paymentCard')))
         pki_builder.append('currency', request.get('currency'))
         pki_builder.append('connectorName', request.get('connectorName'))
-        pki_builder.append('callbackUrl', request.get('callbackUrl'))
         return pki_builder.get_request_string()
 
 
